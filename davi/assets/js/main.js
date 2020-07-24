ba4fe4fb5f99766ab8a5235bae20a9bfb53a50ad
dwiyanscg <dwiyanscahyag@gmail.com>
2020-07-25 06:39:52 +0700

    perubahan

--- davi/assets/js/main.js
+++ davi/assets/js/main.js
@@ -7,7 +7,24 @@
      /* Page Loader active
     ========================================================*/
     $('#preloader').fadeOut();
-
+// gallery
+$('.popup-gallery').magnificPopup({
+  delegate: 'a',
+  type: 'image',
+  tLoading: 'Loading image #%curr%...',
+  mainClass: 'mfp-img-mobile',
+  gallery: {
+    enabled: true,
+    navigateByImgClick: true,
+    preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+  },
+  image: {
+    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    titleSrc: function(item) {
+      return item.el.attr('title') + '<small>by @kemarinsore</small>';
+    }
+  }
+});
   // Sticky Nav
     $(window).on('scroll', function() {
         if ($(window).scrollTop() > 200) {
