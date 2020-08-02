7d514e6213adf96f13e624cf1d0150f1b5470d85
Dwiyan Setia Cahya Gustaman <dwiyanscahyag@gmail.com>
2020-08-02 10:01:49 +0700

    edited

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,11 @@
      /* Page Loader active
     ========================================================*/
     $('#preloader').fadeOut();
+
+    
+  
+   
+
 // gallery
 $('.popup-gallery').magnificPopup({
   delegate: 'a',
